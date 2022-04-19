59049c55025b460e63f18fb7bb7ccb13b7bb99e3
Gema Refantero <refara.08@gmail.com>
2022-04-19 14:14:13 +0700

    applied animation to all component

--- animate.js
+++ animate.js
@@ -1,7 +1,22 @@
-gsap.from(".card", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  stagger: 0.3,
-  ease: "power2.in",
+const t1 = gsap.timeline({
+  defaults: { duration: 0.5, ease: "power1.out" },
 });
+
+t1.from("#navbar", { y: -100, opacity: 0 })
+  .from(
+    "#main",
+    {
+      x: 100,
+      opacity: 0,
+    },
+    "<.5"
+  )
+  .from("#bunch-o-cards", {
+    y: 100,
+    opacity: 0,
+  })
+  .from(".card", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.3,
+  });
